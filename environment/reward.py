22e3e171c15555c5d796c3219f4a772c716709fe
beyzasenoll <beyzasenol08@gmail.com>
2025-06-10 15:52:45 +0300

    resend all files

--- environment/reward.py
+++
@@ -1,7 +0,0 @@
-class Reward:
-    @classmethod
-    def calculate_rewards(cls, action1, action2):
-        """Calculate rewards based on actions."""
-        if action1 == action2:
-            return 1, 1
-        return 0, 0
